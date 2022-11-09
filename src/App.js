0355db303e2753e984d41e78416a5ff1bb80fea1
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2022-11-09 23:41:40 +0600

    router add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <RouterProvider router={router}>
+
+      </RouterProvider>
 
     </div>
   );
