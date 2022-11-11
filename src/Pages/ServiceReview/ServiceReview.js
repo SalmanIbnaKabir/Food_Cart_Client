ebc28a61c02956c204dbd22572df9630add63f54
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2022-11-11 14:48:08 +0600

    don't give up

--- src/Pages/ServiceReview/ServiceReview.js
+++ src/Pages/ServiceReview/ServiceReview.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const ServiceReview = () => {
+  const {user} = useContext(AuthContext);
 
 
   return (
     <div>
       <h1>Review Found</h1>
-      
+      <div>
+
+      </div>
+      <div>
+        <h1>Add Review</h1>
+
+      </div>
       
     </div>
   );
